99e29a66e1f8c47f30271dfd41de47084becc9b2
Vijay Joseph <iamvijayjosephvj007@gmail.com>
2024-12-11 12:32:14 +0530

    Add files via upload

---
+++ 30 project/calander/button animate/Random Image/image.js
@@ -0,0 +1,19 @@
+const containerImageEl=document.querySelector(".container-image");
+const  btnEl=document.querySelector(".btn");
+ 
+btnEl.addEventListener("click",()=>{
+      imageNum=10
+      addNewImages()
+});
+
+function addNewImages(){
+    for (let index = 0; index < imageNum; index++) {
+      
+        const newImgEl=document.createElement("img");
+        newImgEl.src=`https://picsum.photos/300?random=${Math.floor(
+            Math.random()*2000
+        )} `;
+   containerImageEl.appendChild(newImgEl);
+    }
+   
+    }
